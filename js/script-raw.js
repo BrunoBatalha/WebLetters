5b7ea713b6df11cf4d0015c9f9510533598fa64e
Bruno Batalha <brunobatalha37@gmail.com>
2020-09-10 00:41:49 -0400

    Adiciona animações

--- js/script-raw.js
+++ js/script-raw.js
@@ -146,3 +146,32 @@ function criaCarta(remetente, key, elementoPai) {
 function salvaKeyCarta(key) {
   sessionStorage.setItem(CARTA_STORAGE, key);
 }
+
+window.SimpleAnime = class {
+  constructor() {
+    (this.items = document.querySelectorAll("[data-anime]")), this.init();
+  }
+  animateItems() {
+    this.items.forEach(t => {
+      const e = Number(t.getAttribute("data-anime"));
+      isNaN(e) ||
+        setTimeout(() => {
+          t.classList.add("anime");
+        }, e);
+    });
+  }
+  handleVisibility() {
+    void 0 !== document.visibilityState
+      ? "visible" === document.visibilityState && this.animateItems()
+      : this.animateItems();
+  }
+  init() {
+    (this.handleVisibility = this.handleVisibility.bind(this)),
+      this.handleVisibility(),
+      document.addEventListener("visibilitychange", this.handleVisibility);
+  }
+};
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
